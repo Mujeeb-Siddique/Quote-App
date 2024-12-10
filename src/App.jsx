bc2bcea69ebd25393d589975f5c71f009811e6e3
Mujeeb-Siddique <mujeebsiddique9@gmail.com>
2024-12-10 09:17:16 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import QuoteBox from './Components/QuoteBox/QuoteBox'
+
+
+const App = () => {
+  return (
+    <>
+      <div>
+        <QuoteBox/>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/QuoteBox/QuoteBox.jsx
@@ -0,0 +1,52 @@
+/* eslint-disable no-unused-vars */
+import React, { useState, useEffect } from 'react';
+import './QuoteBox.css';
+import { FaShuffle } from "react-icons/fa6";
+
+// Access the API key from .env file
+const apiKey = process.env.REACT_APP_API_KEY;
+
+const QuoteBox = () => {
+  const [quote, setquote] = useState({});
+  
+  const category = 'attitude';
+
+  const getQuote = async () => {
+    try {
+      const response = await fetch(`https://api.api-ninjas.com/v1/quotes?category=${category}`, {
+        headers: { 'X-Api-Key': apiKey } // Use the API key from .env
+      });
+      const data = await response.json();
+      if (data.length > 0) {
+        setquote({
+          quote: data[0].quote,
+          author: data[0].author,
+          category: data[0].category,
+        });
+      }
+    } catch (error) {
+      console.log(error, "Quotes are unavailable right now");
+    }
+  };
+
+  useEffect(() => {
+    getQuote();
+  }, []);
+
+  return (
+    <div className='QuoteBox'>
+      <div className='top'>
+        <p>{quote.quote}</p>
+      </div>
+      <div className='divider'></div>
+      <div className='bottom'>
+        <p className='author'>- Author: {quote.author}</p>
+        <button className='shuffle-btn' onClick={getQuote}>
+          <FaShuffle /> 
+        </button> 
+      </div>
+    </div>
+  );
+};
+
+export default QuoteBox;
